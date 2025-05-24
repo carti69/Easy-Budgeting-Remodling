09f43083bf3886af5361f4b6afa1f8d95352b71f
Carti-linux <zhukow28@gmail.com>
2025-05-23 21:44:54 -0600

    first commit

---
+++ backend/src/main/java/com/example/mavenproj/MavenprojApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenprojApplication.class, args);
+	}
+
+}
