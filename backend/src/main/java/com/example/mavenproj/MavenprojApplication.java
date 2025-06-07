aef2a5c89682fdc04d9a3162d035eb753f632d08
Carti-linux <zhukow28@gmail.com>
2025-06-07 17:53:00 -0600

    added backend message and connected the two, also turned off the need for a database as that will be next.

--- backend/src/main/java/com/example/mavenproj/MavenprojApplication.java
+++ backend/src/main/java/com/example/mavenproj/MavenprojApplication.java
@@ -2,12 +2,15 @@ package com.example.mavenproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @SpringBootApplication
 public class MavenprojApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenprojApplication.class, args);
 	}
-
 }
